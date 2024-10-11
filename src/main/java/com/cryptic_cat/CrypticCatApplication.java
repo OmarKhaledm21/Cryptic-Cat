a973bdba000100822abf6f95923713c32f034b32
Omarkhaledm21 <omarkhaledm21@gmail.com>
2024-10-12 02:40:04 +0300

    INIT PROJECT

---
+++ src/main/java/com/cryptic_cat/CrypticCatApplication.java
@@ -0,0 +1,13 @@
+package com.cryptic_cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrypticCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrypticCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptic_cat/CrypticCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptic_cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrypticCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
